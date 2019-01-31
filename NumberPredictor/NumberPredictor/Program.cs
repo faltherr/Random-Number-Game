bf67ebaf4a538b94c7ded093f3ab7063b8a8f78c
Forest Altherr <faltherr1@gmail.com>
2019-01-30 18:08:44 -0700

    Basic program annotations.

---
+++ NumberPredictor/NumberPredictor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+//Namespace
+namespace NumberPredictor
+{
+    //Main Class
+    class Program
+    {
+        //Entry Point Method
+        static void Main(string[] args)
+        {
+            string name = "Forest Altherr"
+            //Add a new line with hello world
+            Console.WriteLine(name);
+
+        }
+    }
+}
